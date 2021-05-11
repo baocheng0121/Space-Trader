abe21977c62900e8e7708161ee9c29503f8ed039
Baocheng Wang <bwan3675@uni.sydney.edu.au>
2021-05-11 19:40:15 +0800

    progress : can now send GET and POST command to the web

---
+++ src/main/java/DummyAPI/DummyFacade.java
@@ -0,0 +1,14 @@
+package DummyAPI;
+
+public class DummyFacade {
+
+    public String getGameStatus(){
+        return "spacetraders is currently online and available to play";
+    }
+
+
+
+
+
+
+}
